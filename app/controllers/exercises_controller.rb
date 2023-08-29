65ddefac23d3f846961fc917a02d4132b84ef1f7
maiko <urbanrepablic@gmail.com>
2023-08-29 18:10:59 +0900

    first push

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,13 +2,13 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:order_foods).where(order_foods:{order_id: nil})
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop
+    @shops = Shop.left_outer_joins(foods: :order_foods).distinct.where(foods: { order_foods: { id: nil } })
   end
 
   def exercise3 
@@ -16,6 +16,11 @@ class ExercisesController < ApplicationController
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
     @address = Address
+              .joins(:orders)
+              .select('addresses.*, count(orders.*) as orders_count')
+              .group("addresses.id")
+              .order('orders_count desc')
+              .first
   end
 
   def exercise4 
@@ -23,5 +28,10 @@ class ExercisesController < ApplicationController
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
     @customer = Customer
+                .joins(orders: {foods: :order_foods})
+                .select('customers.*,SUM(foods.price) as foods_price_sum')
+                .group("customers.id")
+                .order('foods_price_sum desc')
+                .first
   end
 end
